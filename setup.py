e3dedee061130d9269f78374f3654e1aa371d291
Ksenia <briling@mail.ru>
2022-08-05 19:42:23 +0200

    Add setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='bubbles',
+    version='0.1',
+    description='the ultimate bubble generator',
+    url='https://github.com:briling/bubbles.git',
+    install_requires=[],
+    packages=['bubbles'],
+)
